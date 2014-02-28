9eec53143792c583385f5ecd0342e2b472c1158b
XAMelleOH <xamelleon.net@gmail.com>
2014-02-28 23:37:06 +0200

    improvements

--- rest/js/init.js
+++ rest/js/init.js
@@ -1,9 +1,20 @@
 $(document).ready(function()
 {
 	
-	$('span.question').click(function(){
-		$(this).next().slideToggle();
-	});
+	$(window).scroll(scrl);
+	scrl();
+
+	$('.fa-heart-o').hover(
+		function(){ $(this).addClass('fa-heart').removeClass('fa-heart-o'); },
+		function(){ $(this).removeClass('fa-heart').addClass('fa-heart-o'); }
+	);
 	
-	
+	function scrl(){
+		if ($('.rest').length) {
+			if ($(window).scrollTop() < $('.rest').offset().top)
+				$('.rest-info').css({ top: $('.rest').offset().top - $(window).scrollTop()});
+			else
+				$('.rest-info').css({ top: 0 });
+		}
+	}
 });
