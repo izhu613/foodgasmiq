e4aaba84891e33e1a184ae7caea72b7fe7163307
alexk111 <alexkaul@googlemail.com>
2014-03-01 19:17:29 +0800

    Tasks 1-5
    1) 1200px width for single page view
    2) image margins to global dish view
    3) ajax on global dish view and restaurant view
    4) apply hover-over div to the images on restaurant view
    5) new header

--- rest/js/init.js
+++ rest/js/init.js
@@ -1,20 +1,42 @@
-$(document).ready(function()
-{
-	
-	$(window).scroll(scrl);
-	scrl();
+$(document).ready(function() {
 
 	$('.fa-heart-o').hover(
 		function(){ $(this).addClass('fa-heart').removeClass('fa-heart-o'); },
 		function(){ $(this).removeClass('fa-heart').addClass('fa-heart-o'); }
 	);
-	
-	function scrl(){
-		if ($('.rest').length) {
-			if ($(window).scrollTop() < $('.rest').offset().top)
-				$('.rest-info').css({ top: $('.rest').offset().top - $(window).scrollTop()});
-			else
-				$('.rest-info').css({ top: 0 });
-		}
+
+    function loadLazyImgs() {
+        var curPos=$(window).scrollTop()+$(window).height();
+        $('img').each(function(){
+            var $img=$(this);
+            if($img.data('src')){
+                if($img.offset().top<curPos) {
+                    $img.attr('src',$img.data('src')).data('src','');
+                }
+            }
+        });
+    }
+
+	function scrlRest(){
+		if ($(window).scrollTop() < $('.rest').offset().top)
+			$('.rest-info').css({ top: $('.rest').offset().top - $(window).scrollTop()});
+		else
+			$('.rest-info').css({ top: 0 });
+
+        loadLazyImgs();
 	}
+
+    function scrlGlobal(){
+        loadLazyImgs();
+    }
+
+    if($('.rest').length) {
+        $(window).scroll(scrlRest);
+        scrlRest();
+    }
+
+    if($('.global').length) {
+        $(window).scroll(scrlGlobal);
+        scrlGlobal();
+    }
 });
